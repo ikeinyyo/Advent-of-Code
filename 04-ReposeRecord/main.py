7eab2afd69f7aa9004ce1d4c0b1134a372959d11
maktub82 <gallardo91_@hotmail.com>
2019-04-08 00:14:59 +0200

    Add solution to Day 4, part one

--- 04-ReposeRecord/main.py
+++ 04-ReposeRecord/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 part_one_input = "data/input.txt"
 part_two_input = "data/input.txt"
 
@@ -13,21 +15,58 @@ def parse_date(tokens):
 def parse_time(tokens):
     h = int(tokens[1][:2])
     m = int(tokens[1][3:5])
-    return m if h == 0 else -1
+    return m if h == 0 else 61
 
 def parse_id(tokens):
-    return tokens[3][1:] if tokens[3][0] == '#' else ''
+    return tokens[3][1:] if tokens[3][0] == '#' else None
 
 def parse_status(tokens):
     return 1 if tokens[2] == 'falls' else 0
+
+def create_guards(logs):
+    guards = []
+    for log in logs:
+        if not any(guard['id'] == log['id'] for guard in guards):
+            guards.append({'id': log['id'], 'status': 0, 'last_time': 0, 'times': np.zeros(60)})
+    return guards
+
+def get_guard_by_id(guards, id):
+    return next((guard for guard in guards if guard['id'] == id), None)
+
+def create_report(logs, guards):
+    id = 0
+    for log in logs:
+        if log['id']:
+            id = log['id']
+
+        guard = get_guard_by_id(guards, id)
+
+        if log['id']:
+            guard['last_time'] = 0
+
+        if guard['last_time'] > log['time']:
+            range = guard['times'][guard['last_time']: 59]
+            range[:] = range + 1
+            guard['last_time'] = 0
+
+        if log['status'] == 1 and guard['status'] == 0:
+            guard['last_time'] = log['time']
+        elif log['status'] == 0 and guard['status'] == 1:
+            range = guard['times'][guard['last_time']: log['time']]
+            range[:] = range + 1
+        guard['status'] = log['status']
 
 def part_one():
     logs = []
     for line in open(part_one_input):
         logs.append(parse(line))
     logs.sort(key=lambda log: (log['date'], log['time']))
-    print(logs)
-    print("Day 4")
+
+    guards = create_guards(logs)
+
+    create_report(logs, guards)
+    guards.sort(key=lambda guard: guard['times'].sum(), reverse=True)
+    print("{} x {} = {}".format(guards[0]['id'], np.argmax(guards[0]['times']), (int(guards[0]['id']) * np.argmax(guards[0]['times']))))
 
 def main():
     part_one()
